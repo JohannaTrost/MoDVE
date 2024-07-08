bda4f4c7d4267efb6943a50f7080fed335900008
Makis Kappas <makis.kappas@gmail.com>
2024-07-08 17:05:59 +0100

    Remove hardcoded variables
    Replace them with the ones from the TOML file

--- R/A1.R
+++ R/A1.R
@@ -36,37 +36,37 @@ if (!(file.exists(filepath) && !dir.exists(filepath))) {
 # This parameter determines which type of microhatiat matrices are generated:
 # 1: real GroIMP forest with dynamics
 # 2: static GroIMP forest (only forest at timeStepStart is used)
-MicrohabitatType <- 1
+MicrohabitatType <- config$MicrohabitatType
 
 # Parameters of light model
-kL <- 0.6  # light extinction coefficient
-DistVoxToConsider <- 8  # How many ring around focal voxel to consider in light model (5 voxels in x and y direction)
+kL <- config$kL  # light extinction coefficient
+DistVoxToConsider <- config$DistVoxToConsider  # How many ring around focal voxel to consider in light model (5 voxels in x and y direction)
 
 # Choose the forest parameters that shall be calculated and stored in the microhabitat matrix
 # (this list can be extended for possible new applications of the epiphyte model.
 # 1: use this variable
 # 0: do not use it
-TotalSurfaceAreaOpt <- 1
-SurfaceAreaLossOpt <- 1
-LightConditionsOpt <- 1
-AverageWeightedAngles <- 0
+TotalSurfaceAreaOpt <- config$TotalSurfaceAreaOpt
+SurfaceAreaLossOpt <- config$SurfaceAreaLossOpt
+LightConditionsOpt <- config$LightConditionsOpt
+AverageWeightedAngles <- config$AverageWeightedAngles
 
 # Parameters that need to be specified if MicrohabitatType=1 or MicrohabitatType=2
 # Directory of GroIMP files (this directory is stored in the Microhabitat folder so that the
 # connection to the input GroIMP files is always clear)
-DirectoryGroIMP <- "/PATH/TO/INPUT"
-DirectorySaveMain <- "/PATH/TO/OUTPUT"
-DirectorySaveFolder <- "MicrohabitatMat"
+DirectoryGroIMP <- config$DirectoryGroIMP
+DirectorySaveMain <- config$DirectorySaveMain
+DirectorySaveFolder <- config$DirectorySaveFolder
 
 # Name under which the microhabitat matrices are saved (The name of the folder under
 # which the microhabitat matrices are saved is standarized and only the name
 # of the forest is required here, and only if MicrohabitatType=1 or MicrohabitatType=2)
-NameForest <- "ForestModel_Best"
-ReplicateForest <- 0
+NameForest <- config$NameForest
+ReplicateForest <- config$ReplicateForest
 
 # start and end timestep
-timeStepStart <- 1
-timeStepEnd <- 40
+timeStepStart <- config$timeStepStart
+timeStepEnd <- config$timeStepEnd
 
 # Parameters that need to be specified if MicrohabitatType=3
 # The following parameters are only needed if MicrohabitatType=3
@@ -76,6 +76,8 @@ timeStepEnd <- 40
 # dimyTheoretical <- 50
 # BAI <- 3  # branch area index for the static, theoretical forest
 # LAI <- 6  # leaf are index
+
+# --------------------------------------------------------------------------- #
 
 # Additional parameters
 # Names of essential GroIMP files
