2fb08151a364d11fa5b7a55aca160144e12fcffa
Makis Kappas <makis.kappas@gmail.com>
2024-07-12 14:25:37 +0100

    Move config parser into its own file for reuse

--- R/A1.R
+++ R/A1.R
@@ -1,34 +1,8 @@
 # Create microhabitat matrices
-
-library("optparse")
-library("configr")
+source("utils.R")
 
 
-# Parse command line arguments
-parser <- OptionParser()
-parser <- add_option(parser,
-    c("-i", "--input"),
-    type="character",
-    default=NA,
-    metavar="PATH_TO_TOML",
-    help="Path to TOML formatted input file")
-opt <- parse_args(parser)
-
-# Check that input file is provided
-if (is.na(opt$input)) {
-    stop("Input parameter must be provided. See script usage (--help)")
-} else {
-    filepath <- file.path(opt$input)
-}
-
-# Check whether provided input file exists and is in TOML format
-if (!(file.exists(filepath) && !dir.exists(filepath))) {
-    stop("Input file doesn't exist")
-} else if (!is.toml.file(file=opt$input)) {
-    stop("Input file isn't in TOML format")
-} else {
-    config <- read.config(file=filepath)
-}
+config <- parse_config()
 
 # ------------------- Parameters ----------------------- #
 # Parameters that need to be specified/checked before running this script
