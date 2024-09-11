962f5348265deca08d827c0ff54de8af19b77c99
Makis Kappas <makis.kappas@gmail.com>
2024-09-11 22:31:40 +0100

    Start loop over microhabitats and distributions
    We probably don't need the two outer most loops.
    We can probably submit diferent jobs for different
    microhabitats and initial distributions but need to investigate
    further.

--- R/A4.R
+++ R/A4.R
@@ -46,3 +46,71 @@ MortRateMassScaling <- -0.25  # widely used scaling fator
 DirectoryMicrohabitat <- "path/to/A1/output"  # array???
 DirectorySpeciesPools <- "path/to/A2/output"  # array???
 DirectoryModelResults <- "path/to/output"  # array???
+
+###############################################################################
+
+for (MicrohabitatNumber in 1:length(FolderEpiphyteModels)) {
+    for (InititalDistNumber in 1:length(FolderInitialDistributions)) {
+
+        # Create folder to save the model results
+        dir.create(DirectoryModelResults, recursive=TRUE)
+
+        # Load plot dimensions
+        dimPlot <- readRDS(file.path(DirectoryMicrohabitat, "dimPlot.rds"))
+
+        # Set StopCriterion for this simulation
+        StopCriterion <- StopCriterionHa * dimPlot[1] * dimPlot[2] * 0.0001
+
+        # Load TraitRanges (ranges used to create the species pool)
+        FileTraitRanges <- file.path(DirectorySpeciesPools, "TraitRanges.csv")
+        TraitRanges <- read.table(FileTraitRanges, sep=",", header=FALSE)
+
+        SlopeRecruitment <- TraitRanges[1, 1]
+        InterceptRecruitment <- TraitRanges[2, 1]
+
+
+
+
+
+
+
+
+        ###############################################################################
+        # Main loop for the community model
+
+        for (numPool in numSpeciesPools[1]:numSpeciesPools[2]) {
+
+            # Check if a initial distribution for the species pool exists. If not, move on to the next species pool
+            FileNameInitalDistributionPool <- paste("ID_SpeciesP_", numPool, "_Rep_1", ".csv", sep="")
+            if (!file.exists(file.path(DirectoryModelMain, FileNameInitalDistributionPool))) {
+                break
+            }
+
+            # First step: create probability matrices for each species
+            # Load species pool
+            SpeciesPoolFileName <- paste("SpeciesPool", numPool, ".csv", sep="")
+            SpeciesPool <- read.csv(file.path(DirectorySpeciesPools, SpeciesPoolFileName), sep=",", header=TRUE)
+            NumberOfSpecies <- nrow(SpeciesPool)  # number of species per 25X25m plot
+
+            ###########################################################################
+            # Erzeugen der Distanzmatrix und der Wahrscheinlichkeitsmatrix für jede Art
+            # Dimensionen der Dispersal matrix
+            dimX <- dimPlot[1] * 2 + 1
+            dimY <- dimPlot[2] * 2 + 1
+            dimZ <- dimPlot[3] * 2 + 1
+
+            # Erzeugen der Distanzmatrix mit allen Distanzen zum
+            centralPoint <- c(floor(dimX/2) + 1, floor(dimY/2) + 1, floor(dimZ/2) + 1)
+            DistanceMatrix <- array(rep(0, dimX * dimY * dimZ), dim=c(dimX, dimY, dimZ))
+
+            for (i in 1:dimX) {
+                for (j in 1:dimY) {
+                    for (k in 1:dimZ) {
+                        # DistanceMatrix[i, j, k] <- pdist(rbind(c(i, j, k), c(centralPoint[1], centralPoint[2], centralPoint[3])))
+                    }
+                }
+            }
+
+        }
+    }
+}
