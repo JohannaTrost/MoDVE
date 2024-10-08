4f9ade189db262624a3e6eb1dc04984b1629c50e
Makis Kappas <makis.kappas@gmail.com>
2024-10-08 19:29:05 +0100

    Set rng seed

--- R/A4.R
+++ R/A4.R
@@ -29,6 +29,8 @@
 
 ###############################################################################
 # Parameters that need to be specified/checked before running this script
+seed <- 42
+set.seed(seed)
 
 # Folder of epiphyte models (these models are simulated in this order)
 # The names of the models in the Folder "EpiphyteModels" are needed here
