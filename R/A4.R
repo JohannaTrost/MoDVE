d52d6c05ef0686a2ebf31f61e6b2fe7c6a5dc926
Makis Kappas <makis.kappas@gmail.com>
2024-10-14 14:00:40 +0100

    Reorder input parameters

--- R/A4.R
+++ R/A4.R
@@ -11,16 +11,16 @@
 seed <- 42
 set.seed(seed, kind="Mersenne-Twister")
 
-# Folder of epiphyte models (these models are simulated in this order)
-# The names of the models in the Folder "EpiphyteModels" are needed here
-FolderEpiphyteModels <- c("ForestModel_Best_30x30_Rep0")  # unused?
+# Input directories
+DirectoryMicrohabitat <- "path/to/A1/output"
+DirectorySpeciesPools <- "path/to/A2/output"
+DirectoryModelMain <- "path/to/A3/output"
+
+# Output directory
+DirectoryModelResults <- "path/to/output"
+
 MicrohabitatType <- 1  # Define which type of forest the microhabitat belongs to. 1: dynamic forest, 2: static forest, 3: uniform forest
 SingleSpeciesModel <- 0  # 1: Single species model, 0: Community model
-
-# Choose initial distributions (have to be located in 'FolderEpiphyteModel\IniDist\')
-FolderInitialDistributions <- c("SP_Random_IA_2_IR_70_TimeS_1")  # unused?
-
-DirectoryModelMain <- "path/to/A3/output"
 
 # Model parameters
 timeSteps <- 39  # Model for timeSteps beginning at the time step given by the initial distribution
@@ -46,14 +46,17 @@ MortRateRandom <- 0.1
 MortRateMass <- 0.1
 MortRateMassScaling <- -0.25  # widely used scaling fator
 
-
-# NEW
-# Directories where microhabitat and species pool is saved
-DirectoryMicrohabitat <- "path/to/A1/output"  # array???
-DirectorySpeciesPools <- "path/to/A2/output"  # array???
-DirectoryModelResults <- "path/to/output"  # array???
 InitialTimeStep <- 1  # Time step for which the Initial distribution is generated in A3
 
+###############################################################################
+
+# Check if we can delete the following
+# Choose initial distributions (have to be located in 'FolderEpiphyteModel\IniDist\')
+FolderInitialDistributions <- c("SP_Random_IA_2_IR_70_TimeS_1")  # unused?
+
+# Folder of epiphyte models (these models are simulated in this order)
+# The names of the models in the Folder "EpiphyteModels" are needed here
+FolderEpiphyteModels <- c("ForestModel_Best_30x30_Rep0")  # unused?
 ###############################################################################
 
 compute_prob_matrix_norm <- function(centralPoint, dimX, dimY, dimZ, NumberOfSpecies, SpeciesPool) {
