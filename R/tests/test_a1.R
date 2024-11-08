d5939e899d5a723be2a6c57700a3f3eca7f00c92
Makis Kappas <makis.kappas@gmail.com>
2024-11-08 11:52:53 +0000

    Rename variables and update comment

--- R/tests/test_a1.R
+++ R/tests/test_a1.R
@@ -1,24 +1,27 @@
+# Prior to running this test, execute the A1 script using the input file
+# located in this directory. This will create the necessary output files,
+# which will subsequently be compared against the expected files.
 # Run this test from within the R/ directory to ensure
 # correct interpretation of relative paths within the script.
 
-dir_exp <- "tests/data/microhabitat"
-dir_rec <- "tests/data/output_a1"
+dir_expected <- "tests/data/microhabitat"
+dir_received <- "tests/data/output_a1"
 
 # Test dimPlot
-dimPlot_exp <- readRDS(file.path(dir_exp, "dimPlot.rds"))
-dimPlot_rec <- readRDS(file.path(dir_rec, "dimPlot.rds"))
+dimPlot_expected <- readRDS(file.path(dir_expected, "dimPlot.rds"))
+dimPlot_received <- readRDS(file.path(dir_received, "dimPlot.rds"))
 
-stopifnot(identical(length(dimPlot_exp), length(dimPlot_rec)))
-stopifnot(identical(dimPlot_exp, dimPlot_rec))
+stopifnot(identical(length(dimPlot_expected), length(dimPlot_received)))
+stopifnot(identical(dimPlot_expected, dimPlot_received))
 
 # Test Microhabitat
 Nfiles <- 3
 for (t in seq_len(Nfiles)) {
     microhab_filename <- paste("MicrohabitatMatrix", t, ".rds", sep="")
 
-    microhab_exp <- readRDS(file.path(dir_exp, microhab_filename))
-    microhab_rec <- readRDS(file.path(dir_rec, microhab_filename))
+    microhab_expected <- readRDS(file.path(dir_expected, microhab_filename))
+    microhab_received <- readRDS(file.path(dir_received, microhab_filename))
 
-    stopifnot(identical(dim(microhab_exp), dim(microhab_rec)))
-    stopifnot(isTRUE(all.equal(microhab_exp, microhab_rec, tolerance=1e-5)))
+    stopifnot(identical(dim(microhab_expected), dim(microhab_received)))
+    stopifnot(isTRUE(all.equal(microhab_expected, microhab_received, tolerance=1e-5)))
 }
