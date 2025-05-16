b7c07cb6635be5807b3d2172fe07d7b072444b06
johanna.trost <johanna.trost@univ-lyon1.fr>
2025-05-16 16:26:47 +0200

    Fixed issue with vegfromhab and plotted veg.input vars.

--- R/prepro_mc/01_02_mc_input_veg.R
+++ R/prepro_mc/01_02_mc_input_veg.R
@@ -61,6 +61,10 @@ lcover <- aggregate(lcover, 50, fun = 'mean', na.rm = TRUE)
 # Perform mosaic
 lai <- lai_mosaic(lcover, pathout, reso = 500, msk = TRUE)
 
+# Save lai 
+writeRaster(lai, "/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/lai_2024_500m.tif", 
+            filetype = "GTiff", overwrite = TRUE)
+
 plot(lai)
 lai_4326 <- project(lai, "EPSG:4326")
 lai_cropped <- crop(lai_4326, e)
@@ -73,39 +77,86 @@ library(microclimf)
 # PAI: Plant area index values represent the combined one sided woody and 
 #      green vegetation plant area per unit ground area.
 
-habitats <- rast("/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/ESA_WorldCover_2023_10m.tif")
+hab <- rast("/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/ESA_WorldCover_2023_10m.tif")
 cheight <- rast("/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/canopy_height_2020_1m.tif")
 
-habitats[habitats == 10] <- 2   # Forest
-habitats[habitats == 20] <- 7   # Shrubland
-habitats[habitats == 30] <- 10  # Grassland
-habitats[habitats == 40] <- 13  # Cropland
-habitats[habitats == 50] <- NA  # Build up 
-habitats[habitats == 60] <- NA  # Bare / sparse vegetation
-habitats[habitats == 80] <- NA  # Water
-habitats[habitats == 90] <- 12  # Herbacious wetland
+hab[hab == 10] <- 2   # Forest
+hab[hab == 20] <- 7   # Shrubland
+hab[hab == 30] <- 10  # Grassland
+hab[hab == 40] <- 13  # Cropland
+hab[hab == 50] <- NA  # Build up 
+hab[hab == 60] <- NA  # Bare / sparse vegetation
+hab[hab == 80] <- NA  # Water
+hab[hab == 90] <- 12  # Herbacious wetland
 
 # Remove color table
-coltab(habitats) <- NULL
+coltab(hab) <- NULL
 
 # Save habitats 
-writeRaster(habitats, "/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/habitats.tif", 
+writeRaster(hab, "/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/habitats.tif", 
             filetype = "GTiff", overwrite = TRUE)
 
 # Below not working properly
 
-habitats <- project(habitats, "EPSG:4326")
+hab <- rast("/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/habitats.tif")
+cheight <- rast("/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/canopy_height_2020_1m.tif")
+lai <- rast("/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/lai_2024_500m.tif")
+
+hab <- as.factor(hab)
+hab <- project(hab, "EPSG:4326", method="near")
 cheight <- project(cheight, "EPSG:4326")
-lai <- project(lai, "EPSG:4326", res=res(habitats))
+lai <- project(lai, "EPSG:4326", res=res(hab))
 
 # Extract longitude and latitude sequences
-coords <- as.data.frame(xyFromCell(habitats, 1:ncell(habitats)))
+coords <- as.data.frame(xyFromCell(hab, 1:ncell(hab)))
 
-vegp <- vegpfromhab(habitats, hgts = cheight, pai = lai, 
-                    coords$y, coords$x, tme)
+tme <- as.POSIXlt(c(0:30) * 3600 * 24, origin = "2024-01-01", tz = "UTC") 
+vegp <- vegpfromhab(hab, hgts = cheight, pai = NA, 
+                    -22.2, -42.2, tme)
 
-plot(terra::unwrap(vegp$pai)[[6]])
-plot(terra::unwrap(vegp$leafr))
+# Define output path
+out_dir <- "/Users/johanna/Uni/masterarbeit/figs/mc_input"
+
+# Plot 1: Jan PAI
+png(file.path(out_dir, "jan_pai.png"))
+plot(terra::unwrap(vegp$pai)[[1]], main = "Jan PAI")
+dev.off()
+
+# Plot 2: Vegetation height
+png(file.path(out_dir, "vegetation_height.png"))
+plot(terra::unwrap(vegp$hgt), main = "Vegetation height")
+dev.off()
+
+# Plot 3: Leaf angle coefficient
+png(file.path(out_dir, "leaf_angle_coefficient.png"))
+plot(terra::unwrap(vegp$x), main = "Leaf angle coefficient")
+dev.off()
+
+# Plot 4: Maximum stomatal conductance
+png(file.path(out_dir, "max_stomatal_conductance.png"))
+plot(terra::unwrap(vegp$gsmax), main = "Maximum stomatal conductance")
+dev.off()
+
+# Plot 5: Leaf reflectance
+png(file.path(out_dir, "leaf_reflectance.png"))
+plot(terra::unwrap(vegp$leafr), main = "Leaf reflectance")
+dev.off()
+
+# Plot 6: Canopy clumping factor
+png(file.path(out_dir, "canopy_clumping_factor.png"))
+plot(terra::unwrap(vegp$clump)[[1]], main = "Canopy clumping factor")
+dev.off()
+
+# Plot 7: Leaf diameter
+png(file.path(out_dir, "leaf_diameter.png"))
+plot(terra::unwrap(vegp$leafd), main = "Leaf diameter")
+dev.off()
+
+# Plot 8: Leaf transmittance
+png(file.path(out_dir, "leaf_transmittance.png"))
+plot(terra::unwrap(vegp$leaft), main = "Leaf transmittance")
+dev.off()
+
 
 saveRDS(vegp, 
         "/Users/johanna/Uni/masterarbeit/code/data/mc_input/vegetation/vegp.RDS")
