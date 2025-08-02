592a6e13775d13d839a2f7f8cf68c58082e96df6
johanna.trost <johanna.trost@univ-lyon1.fr>
2025-08-02 18:04:54 +0200

    Script for downloading bias corrected climate data from cmip models (SSP245).

--- R/prepro_mc/download_cmip6/download_cmip_ceda.py
+++ R/prepro_mc/download_cmip6/download_cmip_ceda.py
@@ -2,6 +2,7 @@ import numpy as np
 import xarray as xr
 import requests
 import pandas as pd
+from pathlib import Path
 
 class TokenSession(requests.Session):
     def __init__(self, token):
@@ -18,7 +19,9 @@ def open_opendap_xarray(url, token):
                          backend_kwargs={"session": session})
     return ds
 
-token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI4ZjhmaUpyaUtDY3hmaHhzdU5vazVEekdJdFZ4amhhTWNJa05ZX2U4MnhJIn0.eyJleHAiOjE3NTQxMzk4MTAsImlhdCI6MTc1Mzg4MDYxMCwianRpIjoiM2YwYzU4ZjAtM2VhMy00MzRiLTg2NDItMGRjMTEzZjQ4MGFhIiwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5jZWRhLmFjLnVrL3JlYWxtcy9jZWRhIiwic3ViIjoiMmQxZTEzNGYtN2FjNi00YWQ4LTg1YjQtNmMyMmNkNzg0Y2RmIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic2VydmljZXMtcG9ydGFsLWNlZGEtYWMtdWsiLCJzZXNzaW9uX3N0YXRlIjoiMzU0MDhlY2UtNmRlYy00ZWY4LWI5ODUtNDJlYWU0NTJiMDYyIiwiYWNyIjoiMSIsInNjb3BlIjoiZW1haWwgb3BlbmlkIHByb2ZpbGUgZ3JvdXBfbWVtYmVyc2hpcCIsInNpZCI6IjM1NDA4ZWNlLTZkZWMtNGVmOC1iOTg1LTQyZWFlNDUyYjA2MiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYW1lIjoiSm9oYW5uYSBUcm9zdCIsInByZWZlcnJlZF91c2VybmFtZSI6Imp0MjgxIiwibG9jYWxlIjoiZW4iLCJnaXZlbl9uYW1lIjoiSm9oYW5uYSIsImZhbWlseV9uYW1lIjoiVHJvc3QiLCJlbWFpbCI6ImpvaGFubmEudHJvc3QuMTk5N0BnbWFpbC5jb20ifQ.E0S9wMOQ0MAmXp3U8e9FYHz4KgudUa7peGBs3yLysQDi5iWia5G6bvRT8heeFaO0rrmL2D_-WnKSHzVQEP2nusyZjF2D18b1koN1BB1zAlEGtq7cl68T9jQ89gDIZm9C5OJqHPs3vpldkiW441Y9Ai0bgJxvVdjWJ8S-ZoDojDESg7DQBWj3XER8UWZrlAdZGhUGrRyNSXidVVbcysk-XYF1XtTIbsFD4pNd-Z8Z3CSxNMhcn-Akl5jbOHUnbHIzOBwk1QLTj5nJBBasB5gTaO8xK1W0ZDv_0pY9hz9lGIGa6WKiP6uDFb5mp2zJzgqj3iFKJywggwVCUgvydpOW7Q"
+token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI4ZjhmaUpyaUtDY3hmaHhzdU5vazVEekdJdFZ4amhhTWNJa05ZX2U4MnhJIn0.eyJleHAiOjE3NTQ0MDA4NzYsImlhdCI6MTc1NDE0MTY3NiwianRpIjoiYjQ4ODIyMWItMzAwMi00MDcyLWFkNmQtNjlkYWVhYzNkMWZhIiwiaXNzIjoiaHR0cHM6Ly9hY2NvdW50cy5jZWRhLmFjLnVrL3JlYWxtcy9jZWRhIiwic3ViIjoiMmQxZTEzNGYtN2FjNi00YWQ4LTg1YjQtNmMyMmNkNzg0Y2RmIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoic2VydmljZXMtcG9ydGFsLWNlZGEtYWMtdWsiLCJzZXNzaW9uX3N0YXRlIjoiNTUwNWY5MjktMjQ5ZS00NWQ0LWI0ODgtYjA5MGIwMTFkM2IzIiwiYWNyIjoiMSIsInNjb3BlIjoiZW1haWwgb3BlbmlkIHByb2ZpbGUgZ3JvdXBfbWVtYmVyc2hpcCIsInNpZCI6IjU1MDVmOTI5LTI0OWUtNDVkNC1iNDg4LWIwOTBiMDExZDNiMyIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYW1lIjoiSm9oYW5uYSBUcm9zdCIsInByZWZlcnJlZF91c2VybmFtZSI6Imp0MjgxIiwibG9jYWxlIjoiZW4iLCJnaXZlbl9uYW1lIjoiSm9oYW5uYSIsImZhbWlseV9uYW1lIjoiVHJvc3QiLCJlbWFpbCI6ImpvaGFubmEudHJvc3QuMTk5N0BnbWFpbC5jb20ifQ.MYVOBwOeE7VNHbwRJWLtcpxymm8-hYqX_OmVmZN0FU8s7zDuXok-fmqqTf4hXLnneCaYiNGfYmXAhngL54pmW0tP65zeelbVjUOL5lVsYBqYlEQy5PPNBadZ2RKfwJpYfA2W4xyvoBxEJ7DEENl0McG0GC5srej9boyAbto-wwlgEKGAePaMSRKFUHwspzLoikH-hTtLbuQ5pnsHtIKN2o71re2bynt7RbvrfycN1ZX1rhUhVa8-IUqmUmV26bxlxCJaQAjqEF_1HvwSsL0ZjP2OECEkyIkGaAUNA4s8qpDz93CPJS8l_xWfBvEuStR7QEx8W59V677guKXcoC0U0w"
+
+out_dir = Path("../../data/mc_input/climate/cmip6_ceda")
 
 # Configure the area of interest and dates
 area = {"lat": [-42.9, -42.2], "lon": [-22.8, -22.2]}
@@ -29,8 +32,8 @@ end = "2024-12-30"
 # Temperatire: FIO-ESM-2-0, CanESM5 and HadGEM3-GC31-MM -> none of them available in CEDA data (https://doi.org/10.5285/c107618f1db34801bb88a1e927b82317, https://doi.org/10.1038/s41597-023-02528-x)
 
 models = {
-    "temperature": ["CMCC-ESM2", "CMCC-CM2-SR5", "MRI-ESM2-0"],
-    "precipitation": ["IPSL-CM6A-LR", "INM-CM4-8", "GFDL-ESM4"],
+    "tas": ["CMCC-ESM2", "CMCC-CM2-SR5", "MRI-ESM2-0"],
+    "hurs": ["IPSL-CM6A-LR", "INM-CM4-8", "GFDL-ESM4"],
 }
     # "HadGEM3-GC31-MM", -> Not available in CEDA
     # "CMCC-ESM2", # Best for temp
@@ -43,41 +46,97 @@ models = {
     # "INM-CM4-8  ", # Best for precipitation in LPB
     #"GFDL-ESM4" # Best for precipitation in LPB
 
-gregorian_lst = [False, True, ]
-
-# Extract correct indices for latitude and longitude
-lats = np.linspace(-90, 90, 580)
-lons = np.linspace(-180, 180, 1421)
-
-lat_idx_range = [np.argmin(np.abs(lat - lats)).astype(int) for lat in area["lat"]]
-lon_idx_range = [np.argmin(np.abs(lon - lons)).astype(int) for lon in area["lon"]]
+# calendar categories gregorian, no_leap_days, 360_days
+calendars = {"tas": ["no_leap_days", "no_leap_days", "gregorian"],
+             "hurs": ["gregorian", "no_leap_days", "no_leap_days"]}
 
 # - Extract time indices
-if gregorian:
-    date_range = pd.date_range(start="2015-01-01", end="2100-12-31", freq="D")
-    dates = date_range.strftime("%Y-%m-%d").to_numpy()
+date_range = pd.date_range(start="2015-01-01", end="2100-12-31", freq="D")
 
+dates_gregorian = date_range.strftime("%Y-%m-%d").to_numpy()
+filtered_range = date_range[
+        ~((date_range.month == 2) & (date_range.day == 29))]
+dates_no_leap_days = filtered_range.strftime("%Y-%m-%d").to_numpy()
 years = range(2015, 2101)  # Inclusive
 months = range(1, 13)
 days = range(1, 31)  # 30 days per month
-dates = np.asarray(
+dates_360days = np.asarray(
     [f"{year:04d}-{month:02d}-{day:02d}"
      for year in years
      for month in months
      for day in days]
 )
-date_idx_range = [np.where(dates == start)[0][0], np.where(dates == end)[0][0]]
 
-subset_str = f"[{date_idx_range[0]}:1:{date_idx_range[1]}][{lat_idx_range[0]}:1:{lat_idx_range[1]}][{lon_idx_range[0]}:1:{lon_idx_range[1]}]"
-url3 = f"dap2://dap.ceda.ac.uk/thredds/dodsC/badc/evoflood/data/Downscaled_CMIP6_Climate_Data/hurs/SSP245/Global_hurs_Downscaled_{model}_2015-2100_ssp245_compressed.nc?hurs{subset_str}"
+date_idx_range = {
+    "gregorian": [np.where(dates_gregorian == start)[0][0], np.where(dates_gregorian == end)[0][0]],
+    "no_leap_days": [np.where(dates_no_leap_days == start)[0][0], np.where(dates_no_leap_days == end)[0][0]],
+    "360_days": [np.where(dates_360days == start)[0][0], np.where(dates_360days == end)[0][0]]
+}
 
-print(subset_str)
+# Dates in date format
+start_date = pd.to_datetime(start)
+end_date = pd.to_datetime(end)
+datetime_range = pd.date_range(start=start_date, end=end_date, freq='D')
 
-session = TokenSession(token)
-ds = open_opendap_xarray(url3, token)
+# Extract correct indices for latitude and longitude
+lats = np.linspace(-90, 90, 580)
+lons = np.linspace(-180, 180, 1421)
+lat_idx_range = [np.argmin(np.abs(lat - lats)).astype(int) for lat in area["lat"]]
+lon_idx_range = [np.argmin(np.abs(lon - lons)).astype(int) for lon in area["lon"]]
+# Regional coordinates
+area_coords = {
+    "lat": lats[lat_idx_range[0]: lat_idx_range[1] + 1],
+    "lon": lons[lon_idx_range[0]: lon_idx_range[1] + 1]
+}
 
+for var in models.keys():
+    print(var)
+    ds_lst = []
+    for i, model in enumerate(models[var]):
 
+        calendar = calendars[var][i]
+        subset_str = (f"[{date_idx_range[calendar][0]}:1:{date_idx_range[calendar][1]}]"
+                      f"[{lat_idx_range[0]}:1:{lat_idx_range[1]}]"
+                      f"[{lon_idx_range[0]}:1:{lon_idx_range[1]}]")
+        url3 = f"dap2://dap.ceda.ac.uk/thredds/dodsC/badc/evoflood/data/Downscaled_CMIP6_Climate_Data/{var}/SSP245/Global_{var}_Downscaled_{model}_2015-2100_ssp245_compressed.nc?{var}{subset_str}"
 
-full_url = "https://dap.ceda.ac.uk/thredds/dodsC/badc/evoflood/data/Downscaled_CMIP6_Climate_Data/hurs/SSP245/Global_hurs_Downscaled_HadGEM3-GC31-LL_2015-2100_ssp245_compressed.nc?hurs"
-session = TokenSession(token)
-ds = open_opendap_xarray(full_url, token)
+        print(subset_str)
+
+        session = TokenSession(token)
+        ds = open_opendap_xarray(url3, token)
+
+        # Linear interpolation over time for missing days to align calendars
+        if calendar == "no_leap_days":
+            all_dates = pd.to_datetime(dates_no_leap_days)
+            target_dates = all_dates[
+                (all_dates >= start_date) & (all_dates <= end_date)]
+
+            assert len(target_dates) == ds["time"].shape[0], "Different number of dates than in ds."
+
+            # Assign these dates to the dataset's time coordinate
+            ds = ds.assign_coords(time=target_dates)
+            # Reindex and interpolate missing days
+            var_filled = (
+                ds[var]
+                .reindex(time=datetime_range)  # Add Feb 29 (or others if missing)
+                .interpolate_na(dim='time', method='linear')
+            # Linear interpolation over time
+            )
+            # Assign interpolated values back to a dataset (if needed)
+            ds = var_filled.to_dataset(name=var)
+        else:
+            ds = ds.assign_coords(time=datetime_range)
+
+        # Assign proper decimal degrees for longitude
+        ds = ds.assign_coords(lat=area_coords["lat"])
+        ds = ds.assign_coords(lon=area_coords["lon"])
+
+        ds_lst.append(ds[var].expand_dims(model=[model]))
+
+    # Concatenate along the new 'model' dimension and turn into a Dataset
+    models_ds = xr.concat(ds_lst, dim='model').to_dataset(name=var)
+
+    # Save the dataset to a netCDF file
+    output_path = out_dir / f"baf_{var}_day_ssp245_{start}_{end}.nc"
+    models_ds.to_netcdf(output_path, mode='w', format='NETCDF4')
+    print(f"Saved {var} data to {output_path}")
