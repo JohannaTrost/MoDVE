d3c25e2d87648522fb339d34812ffbc7f566b713
johanna.trost <johanna.trost@univ-lyon1.fr>
2025-08-11 18:02:45 +0200

    Script to debug new version of micropoint runmodel function.

--- R/prepro_mc/debug_runprofilemodel.R
+++ R/prepro_mc/debug_runprofilemodel.R
@@ -1,4 +1,4 @@
-remotes::install_local("/Users/johanna/Uni/masterarbeit/code/micropoint",
+remotes::install_local("/Users/johanna/Uni/masterarbeit/code/forks/micropoint",
                        force = TRUE)
 
 library(micropoint)
@@ -179,8 +179,9 @@ in_dir <- "/Users/johanna/Uni/masterarbeit/data/mc_input/regua"
 vegp_reg <- readRDS(paste(in_dir, "vegp_mof3d_ptm_v3.RDS", sep = "/"))
 soilc_reg <- readRDS(paste(in_dir, "soilc_v2.RDS", sep = "/"))
 climdata_reg <- read_csv(paste(in_dir, "era5_climdata_2024_v2.csv", sep = "/"))
-microhab_file <- "/Users/johanna/Uni/masterarbeit/code/output/modev_zach_25_01_07/MicrohabitatMatrix99.rds"
-pai <- readRDS(microhab_file)[,,,5]
+#microhab_file <- "/Users/johanna/Uni/masterarbeit/code/output/modev_zach_25_01_07/MicrohabitatMatrix99.rds"
+microhab_file <- "/Users/johanna/Uni/masterarbeit/data/a1_1_output/a1/MicrohabitatMatrix199.rds"
+pai <- readRDS(microhab_file)[,,,4]
 
 # Veg heights
 max_veg_height <- max(terra::values(terra::unwrap(vegp_reg$h)), na.rm = TRUE)
@@ -202,18 +203,121 @@ paii <- pai[x, y, 1:max_hgt]
 start <- Sys.time()
 
 #pmout <- runprofilemodel(climdata_reg, vegparams, paii = paii, grndparams, lat = lat, long= lon)
+pmout <- micropoint::runpointmodel(climdata_reg, reqhgt = heights, vegparams,
+                                   paii, grndparams, lat = lat, long = lon)
 
 end <- Sys.time()
-print(paste("Time taken for runprofilemodel:", end - start))
+print(paste("Time taken for runprofilemodel:", end - start)) # 1.18175451755524 minutes
+
+orig_mout <- pmout
 
 start <- Sys.time()
-mout_fp <- micropoint::runpointmodel(climdata_reg, reqhgt = NA, vegparams,
-                                  paii, grndparams, lat = lat, long = lon)
+
+for (i in seq(length(heights))) {
+  h <- heights[i]
+  print(h)
+  mout_fp <- micropoint::runpointmodel(climdata_reg, reqhgt = h, vegparams,
+                                   paii, grndparams, lat = lat, long = lon)
+
+  for (v in names(pmout)[1:length(pmout)-1]) {
+    orig_mout[[v]][, i] <- mout_fp[[v]]
+  }
+  orig_mout[[v]][i] <- h
+}
+
 end <- Sys.time()
-print(paste("Time taken for runprofilemodel:", end - start))
+print(paste("Time taken for runprofilemodel:", end - start)) # 3.68026636838913 min
+
+saveRDS(orig_mout, file = "../../output/test_debug_micropoint_output_regua.RDS")
 
 # Compare results
-ppout <- plotprofile(climdata_reg, hr = 4029, "tair",
-                     vegparams, paii = paii, grndparams, lat = lat, long= lon)
+# ppout <- plotprofile(climdata_reg, hr = 4029, "tair",
+#                      vegparams, paii = paii, grndparams, lat = lat, long= lon)
 
-plot(mout_fp$heights ~ mout_fp$tair[4029,])
+
+avg_orig <- apply(orig_mout$relhum, 2, mean, na.rm = TRUE)
+avg_pm <- apply(pmout$relhum, 2, mean, na.rm = TRUE)
+
+pdf("../../figs/debug_micropoint_avgrelhum_v2.pdf")
+plot(pmout$heights ~ avg_pm, col = "red", type = "l")
+lines(orig_mout$heights ~ avg_orig)
+dev.off()
+
+######################## DEBUG 2. version
+
+# Simulate one iteration's data:
+x <- 22
+y <- 15
+
+coords <- indices2coords(x, y, terra::unwrap(vegp_reg$pai))[c("x", "y")]
+lon <- coords[[1]]
+lat <- coords[[2]]
+
+vegparams <- extract_params(vegp_reg, lon, lat)
+grndparams <- extract_params(soilc_reg, lon, lat)
+max_hgt <- max(values(terra::unwrap(vegp_reg$h)), na.rm = TRUE)
+paii <- pai[x, y, 1:max_hgt]
+
+heights <- seq(0.5, length(paii))
+
+pmout <- micropoint::runpointmodel(
+  climdata_reg, reqhgt = heights, vegparams,
+  paii, grndparams, lat = lat, long = lon
+)
+
+timestamps <- seq(ymd_h(paste0(year, "-01-01 00")), ymd_h(paste0(year, "-12-31 23")), by = "hour")
+res <- list()
+
+for (i in seq_along(heights)) {
+  h <- heights[i]
+  immediateMessage(paste(x, y, "height:", h))
+  print(h)
+
+  mout <- micropoint::runpointmodel(climdata_reg, reqhgt = h, vegparams,
+                                    paii, grndparams, lat = lat, long = lon)
+
+  immediateMessage("Point model run completed.")
+
+  # Which variables to save
+  if (i == 1 & x == 1 & y == 1) {
+    exp_vars <- c("tair", "tcanopy", "relhum", "windspeed", "obs_time")
+  } else {
+    exp_vars <- c("tair", "tcanopy", "relhum", "windspeed")
+  }
+
+  for (var in exp_vars) {
+    message(paste(x, y, var, h))
+    if (i == 1) {
+      res[[var]] <- array(NA, dim = c(length(heights), length(mout[[var]])))
+    }
+    res[[var]][i, ] <- mout[[var]]
+  }
+}
+
+# aggregate yearly microclimate data
+agg_res <- aggregate_mc(res, timestamps, n_temp_metrics)
+
+# Extract x values from all three line data sources
+x2 <- agg_res$data[1:length(heights)-1, 1] # 1 airt , 7 relhum, 11 windspeed
+#x3 <- apply(pmout$profile[,,2], 2, mean)
+x3 <- apply(pmout$tair, 2, mean)
+
+# Determine the global x-axis range
+x_range <- range(c(x2, x3), na.rm = TRUE)
+y_range <- range(c(heights, seq(length(pmout$height))), na.rm = TRUE)
+
+pdf(paste0("../../figs/mc_output/test_profile_tair_old_vs_new_comp_", x, "_", y, "_v2.pdf"))
+
+# Plot the first line with axis labels
+plot(heights[1:length(heights)-1] ~ x2, type = "l", xlim = x_range, ylim = y_range,
+     xlab = "Temperature (°C)", ylab = "Height (m)")
+
+# Add the second line
+lines(heights[1:length(heights)-1] ~ x3, col = "red")
+
+# Add a legend
+legend("topright",
+       legend = c("Original 'runmodel' for each height", "New cpp 'runmodelProfile'"),
+       col = c("black", "red"), lty = 1, bty = "n")
+
+dev.off()
