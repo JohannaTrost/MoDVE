72a868f8b3774878875c2ec9a90b860f58ae0d67
johanna.trost <johanna.trost.1997@gmail.com>
2026-06-29 16:54:03 +0200

    Clean skript for cropping and formatting data of forest stand.

--- preprocessing/microclimate/02_surface_inputs/04_format_subregion.R
+++ preprocessing/microclimate/02_surface_inputs/04_format_subregion.R
@@ -11,27 +11,20 @@ library(tidyr)
 library(circular)
 library(lubridate)
 
-# Function to insert Feb 29 by extrapolation
-insert_feb29 <- function(df, year) {
-  # Build Feb 29 sequence
-  feb29_times <- seq(ymd_hms(paste0(year, "-02-29 00:00:00")),
-                     ymd_hms(paste0(year, "-02-29 23:00:00")), by = "1 hour")
 
-  # Use Feb 28 and Mar 1 as neighbors
-  feb28 <- df %>% filter(month(obs_time) == 2, day(obs_time) == 28)
-  mar01 <- df %>% filter(month(obs_time) == 3, day(obs_time) == 1)
+# CONFIGURE Region
+REGION <- "regua"
+# Center point of forest stand
+lat <- dms_to_decimal("22°23′44.75″S")
+lon <- dms_to_decimal("42°44′15.78″W")
 
-  # Simple average interpolation (between Feb 28 and Mar 1)
-  feb29 <- feb28
-  feb29$obs_time <- feb29_times
-  num_cols <- sapply(feb29, is.numeric)
-  feb29[num_cols] <- (feb28[num_cols] + mar01[num_cols]) / 2
+# For Pirineus uncomment below
+# REGION <- "pirineus"
+# Center point of forest stand
+# lat <- dms_to_decimal("22°26′46.74″S")
+# lon <- dms_to_decimal("42°30′06.16″W")
 
-  # Bind into full dataset
-  bind_rows(df, feb29) %>% arrange(obs_time)
-}
-
-
+# Function to crop Spatratser object to given region
 terra_crop <- function(data, ext, res = NULL) {
   data_unpacked <- lapply(data, terra::unwrap)
   
@@ -73,31 +66,22 @@ dms_to_decimal <- function(dms) {
   return(sign * (parts[1] + parts[2] / 60 + parts[3] / 3600))
 }
 
+# Directories
+figs_dir <- file.path("..", "modve_figs", "mc_input")
+data_dir <- file.path("..", "modve_data")
+veg_dir <- file.path(data_dir, "mc_input", "vegetation")
+soil_dir <- file.path(data_dir, "mc_input", "soil")
+out <- file.path(data_dir, REGION)
+
+if(!dir.exists(out)) {
+  dir.create(file.path(out))
+}
+
 # Load data
-vegp_baf <- readRDS("/Users/johanna/Uni/masterarbeit/data/mc_input/vegetation/vegp.RDS")
-dtm_baf <- rast("/Users/johanna/Uni/masterarbeit/data/mc_input/soil/dtm.tif")
-soilc_baf <- readRDS("/Users/johanna/Uni/masterarbeit/data/mc_input/soil/soilc_ptm.RDS")
+vegp_baf <- readRDS(veg_dir, "vegp.RDS")
+soilc_baf <- readRDS(soil_dir, "soil.RDS")
 
-# -- REGUA
-#
-# out <- "/Users/johanna/Uni/masterarbeit/data/mc_input/regua"
-# dir.create(file.path(out))
-#
-# # Define the center point - 22°23′44.75″S, 42°44′15.78″W
-# lat <- dms_to_decimal("22°23′44.75″S")
-# lon <- dms_to_decimal("42°44′15.78″W")
-# center_coords <- st_sfc(st_point(c(lon, lat)), crs = 4326)
-# center_proj <- st_transform(center_coords, crs = 31983)
-# center_coords_m <- st_coordinates(center_proj)
-
-# -- Pirineus
-#
-out <- "/Users/johanna/Uni/masterarbeit/data/mc_input/pirineus"
-dir.create(file.path(out))
-
-# Define the center point
-lat <- dms_to_decimal("22°26′46.74″S")
-lon <- dms_to_decimal("42°30′06.16″W")
+# Project coordinates
 center_coords <- st_sfc(st_point(c(lon, lat)), crs = 4326)
 center_proj <- st_transform(center_coords, crs = 31983)
 center_coords_m <- st_coordinates(center_proj)
@@ -105,16 +89,18 @@ center_coords_m <- st_coordinates(center_proj)
 # Create a 50m x 50m square (i.e., a 50m buffer in all directions)
 extent_box <- st_buffer(center_proj, dist = 25, endCapStyle = "SQUARE")
 extent_vect <- vect(extent_box)
+
 # Ceil the extent if decimals
 e <- ext(extent_vect)
 ce <- ext(round(e[1]), round(e[2]), round(e[3]), round(e[4]))
+
 # Create new polygon from ceiled extent
 ceiled_box <- as.polygons(ce)
 crs(ceiled_box) <- crs(extent_vect)  # Set CRS to match original
 
-# -  Prep Vegetation
+# --------------------------- Prep Vegetation --------------------------- #
 
-# Correct layer names 
+# Correct layer names (turns multi-layer SpatRaster into a named list of single-layer SpatRaster objects)
 layer_names <- names(vegp_baf)
 unwrapped_list <- setNames(
   lapply(layer_names, function(nm) {
@@ -125,14 +111,12 @@ unwrapped_list <- setNames(
   layer_names
 )
 vegp_square <- terra_crop(unwrapped_list, ceiled_box, res = 1)
-plot(terra::unwrap(vegp_square$hgt))
 
-# - Prep Soil data
+# --------------------------- Prep Soil data --------------------------- #
+
 soil_square <- terra_crop(soilc_baf, ceiled_box, res = 1)
 
-plot(terra::unwrap(soil_square$soiltype))
-
-# Potentially fill up soil variables if there are NAs
+# Potentially fill soil variables if there are NAs
 for (name in names(soil_square)) {
 
   raster <- terra::unwrap(soil_square[[name]])
@@ -153,55 +137,35 @@ for (name in names(soil_square)) {
   }
 }
 
+# Fix NAs in ground radiation
+soil_square$gref <- terra::ifel(is.na(soil_square$gref), unique(soil_square$gref)[[1]], soil_square$gref)
+
 # Wrap unwrapped soil variables
 soil_square$gref <- terra::wrap(soil_square$gref)
 soil_square$slope <- terra::wrap(soil_square$slope)
 soil_square$aspect <- terra::wrap(soil_square$aspect)
 soil_square$em <- terra::wrap(soil_square$em)
 
-# - Prep DTM
-em <- deepcopy(terra::unwrap(soil_square$em))
-dtm_baf_res <- resample(dtm_baf, em) 
-dtm_square <- dtm_baf_res$lyr1
-
-plot(dtm_square)
+# Correct names for Micropoint
+soil_square <- soil_square[names(micropoint::groundparams)]
 
 # Save data 
-saveRDS(vegp_square, paste(out, "vegp_v2.RDS", sep = "/"))
-writeRaster(dtm_square, paste(out, "dtm_v2.tif", sep = "/"),
-            filetype = "GTiff", overwrite = TRUE)
-saveRDS(soil_square, paste(out, "soilc_v2.RDS", sep = "/"))
+saveRDS(vegp_square, file.path(out, "vegp.RDS"))
+saveRDS(soil_square, file.path(out, "soil.RDS"))
 
-# Visualize data:
-vegp_reg <- readRDS(paste(out, "vegp_v2.RDS", sep = "/"))
-dtm_reg <- rast(paste(out, "dtm_v2.tif", sep = "/"))
-soilc_reg <- readRDS(paste(out, "soilc_v2.RDS", sep = "/"))
-climdata_reg <- read_csv(paste(out, "cmip6_climdata_2024_v1.csv", sep = "/"))
+# -- Visually check data
 
 # Vegetation
-for (name in names(vegp_reg)) {
-  vegp_var <- terra::unwrap(vegp_reg[[name]])
+for (name in names(vegp_square)) {
+  vegp_var <- terra::unwrap(vegp_square[[name]])
   plot(vegp_var, main = name)
 }
-
 print(res(vegp_var))
-
-# DTM
-plot(dtm_reg, main = "dtm")
 
 # Soil
-for (name in names(soilc_reg)) {
-  vegp_var <- terra::unwrap(soilc_reg[[name]])
-  plot(vegp_var, main = name)
+for (name in names(soil_square)) {
+  soil_var <- terra::unwrap(soil_square[[name]])
+  plot(soil_var, main = name)
 }
-print(res(vegp_var))
-
-# Issue with ground radiation
-soilc_reg_unpacked <- lapply(soilc_reg, terra::unwrap)
-soilc_reg_unpacked$gref <- terra::ifel(is.na(soilc_reg_unpacked$gref),
-                                          unique(soilc_reg_unpacked$gref)[[1]],
-                                          soilc_reg_unpacked$gref)
-soic_reg <- lapply(soilc_reg_unpacked, terra::wrap)
-soic_reg <- soic_reg[names(micropoint::groundparams)]
-saveRDS(soic_reg, paste(out, "soilc_v2.RDS", sep = "/"))
+print(res(soil_var))
 
