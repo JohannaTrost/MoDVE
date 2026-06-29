2dbc68ffa88277c57ac5e8f6e3639011dabec716
johanna.trost <johanna.trost.1997@gmail.com>
2026-06-29 16:22:20 +0200

    Clean skript for soil processing.

--- preprocessing/microclimate/02_surface_inputs/03_soil.R
+++ preprocessing/microclimate/02_surface_inputs/03_soil.R
@@ -1,9 +1,7 @@
+# Generate soil parameters and ground reflectance
+
 library(terra)
 library(httr)
-
-# library(remotes)
-# remotes::install_local("/Users/johanna/Uni/masterarbeit/code/microclimdata")
-
 library(microclimdata)
 library(micropoint)
 
@@ -13,21 +11,18 @@ get_by_soiltype <- function(soil_raster) {
   soilparams <- micropoint::soilparams
   
   # Exclude specific columns
-  exclude_vars <- c("Soil.type")
+  exclude_vars <- "Soil.type"
   target_vars <- setdiff(colnames(soilparams), exclude_vars)
   
-  # soil type codes in raster correspond to rows in soilparams:
-  soilparams$soil_code <- 1:nrow(soilparams)
+  # Soil type codes in raster correspond to rows in soilparams:
+  soilparams$soil_code <- seq_len(nrow(soilparams))
   
   # Create a raster for each variable in soilparams
   raster_list <- list()
   for (var in target_vars) {
-    # Create a vector to map soil type code to variable value
-    map_values <- setNames(soilparams[[var]], soilparams$soil_code)
-    
+
     # Reclassify the raster
-    new_raster <- classify(soil_raster, 
-                           rcl = cbind(soilparams$soil_code, soilparams[[var]]))
+    new_raster <- classify(soil_raster, rcl = cbind(soilparams$soil_code, soilparams[[var]]))
     names(new_raster) <- var # Set layer name
     
     # Add to list
@@ -37,10 +32,31 @@ get_by_soiltype <- function(soil_raster) {
   return(raster_list)
 }
 
+# Directories
+figs_dir <- file.path("..", "modve_figs", "mc_input")
+data_dir <- file.path("..", "modve_data")
+soil_dir <- file.path(data_dir, "mc_input", "soil")
+veg_dir <- file.path(data_dir, "mc_input", "vegetation")
+albedo_dir <- file.path(data_dir, "mc_input", "albedo")
 
-albedo <- rast("/Users/johanna/Uni/masterarbeit/data/mc_input/vegetation/albedo/albedo_modis_2024-01-01.tif")
-lai <- rast("/Users/johanna/Uni/masterarbeit/data/mc_input/vegetation/lai_2024_500m.tif")
-landcover <- rast("/Users/johanna/Uni/masterarbeit/data/mc_input/vegetation/ESA_WorldCover_2023_10m.tif")
+# Check and create directories
+if(!dir.exists(soil_dir)) {
+  dir.create(soil_dir)
+}
+if(!dir.exists(figs_dir)) {
+  dir.create(figs_dir)
+}
+if(!dir.exists(veg_dir)) {
+  stop("Run 01_vegetation.R to obtain required vegetation data.")
+}
+if(!dir.exists(albedo_dir)) {
+  stop("Run 02_albedo.R to obtain required albedo data.")
+}
+
+# Load data
+albedo <- rast(file.path(albedo_dir, "albedo_modis_2024-01-01.tif"))
+lai <- rast(file.path(veg_dir, "lai_2024_500m.tif"))
+landcover <- rast(file.path(veg_dir, "ESA_WorldCover_2023_10m.tif"))
 
 # Create a template raster with 100 m resolution
 e <- ext(-42.9, -42.2, -22.8, -22.2)  # xmin, xmax, ymin, ymax
@@ -53,39 +69,24 @@ albedo_100m <- resample(albedo, template, method="bilinear")
 lai_100m <- resample(lai, template, method="bilinear")         
 landcover_100m <- resample(landcover, template, method="near")
 
+# Derive leaf and ground reflectance
 x <- x_calc(landcover_100m, lctype = "ESA")
 x <- mask(x, lai_100m)
-
-# Derive leaf and ground reflectance
 refldata <- reflectance_calc(albedo_100m, lai_100m, x, plotprogress=FALSE)
 
-# Plot outputs
-par(mfrow = c(1, 2))
-plot(refldata$gref, col=gray.colors(255), range = c(0, 1), main = "Ground")
-plot(refldata$lref, col=gray.colors(255), range = c(0, 1), main = "Leaf")
+# Save reflectance data
+writeRaster(refldata$gref, file.path(soil_dir, "ground_ref_100m.tif"), filetype = "GTiff", overwrite = TRUE)
+writeRaster(refldata$lref, file.path(soil_dir, "leaf_ref_100m.tif"), filetype = "GTiff", overwrite = TRUE)
 
-writeRaster(refldata$gref, "/Users/johanna/Uni/masterarbeit/data/mc_input/soil/ground_ref_100m.tif", 
-            filetype = "GTiff", overwrite = TRUE)
-writeRaster(refldata$lref, "/Users/johanna/Uni/masterarbeit/data/mc_input/soil/leaf_ref_100m.tif", 
-            filetype = "GTiff", overwrite = TRUE)
-
-r <- rast("/Users/johanna/Uni/masterarbeit/data/mc_input/vegetation/rgb_cir_2024-02-01/cir.tif")[[1]]
-soildata <- soildata_download(r, 
-                              pathdir = "/Users/johanna/Uni/masterarbeit/data/mc_input/tmp/", 
-                              deletefiles = TRUE)
-
+# Download soil data using microclimdata package
+r <- rast(file.path(veg_dir, "rgb_cir_2024-02-01", "cir.tif"))
+soildata <- soildata_download(r[[1]], pathdir = file.path(data_dir, "tmp"), deletefiles = TRUE)
 
 # --- Generate input data for micropoint model
 
-# TODO get the data at this point
-
-# --- Generate data for microclimf (grid model)
-
+# Generate data for microclimf (grid model)
 soilc <-  create_soilgrid(soildata, refldata, landcover, water = 80)
 attributes(soilc)
-
-# Loading data instead of computing the above / comment if necessary
-soilc <- readRDS("/Users/johanna/Uni/masterarbeit/data/mc_input/soil/soilc.RDS")
 
 # Adapt name for point model
 names(soilc)[names(soilc) == "groundr"] <- "gref"
@@ -105,8 +106,10 @@ for (var_name in names(soilp)) {
 # Rename for correct structure for pointmodel
 names(ptm_soilc)[names(ptm_soilc) == "psi_e"] <- "Psie"
 
+# Get DTM
+dtm <- dem_download(r) # from CIR
+
 # Get additional data for point model
-dtm <- rast("/Users/johanna/Uni/masterarbeit/data/mc_input/soil/dtm.tif")
 ptm_soilc$slope <- terra::terrain(dtm$lyr1,'slope')
 ptm_soilc$aspect <- terra::terrain(dtm$lyr1,'aspect')
 
@@ -122,32 +125,35 @@ ptm_soilc <- ptm_soilc[var_order]
 # Wrap all rasters
 ptm_soilc_wrp <- lapply(ptm_soilc, terra::wrap)
 
-saveRDS(ptm_soilc_wrp, 
-        "/Users/johanna/Uni/masterarbeit/data/mc_input/soil/soilc_ptm.RDS")
+# Save soil data
+saveRDS(ptm_soilc_wrp, file.path(soil_dir, "soil.RDS"))
 
-# SAve plots
-out_dir <- "/Users/johanna/Uni/masterarbeit/figs/mc_input"
+# --- Visualize data
 
-png(file.path(out_dir, "soiltype.png"))
-plot(terra::unwrap(soilc$soiltype), main = "Soil Types")
+png(file.path(figs_dir, "soiltype.png"))
+plot(terra::unwrap(ptm_soilc$soiltype), main = "Soil Types")
 dev.off()
 
-png(file.path(out_dir, "groundr.png"))
-plot(terra::unwrap(soilc$groundr), main = "Ground Reflectance")
+png(file.path(figs_dir, "dtm.png"))
+plot(dtm[[1]])
 dev.off()
 
-png(file.path(out_dir, "rho.png"))
-plot(terra::unwrap(soilc$rho), main = expression("Bulk Density (Mg/m"^3*")"))
+png(file.path(figs_dir, "groundr.png"))
+plot(terra::unwrap(ptm_soilc$gref), col=gray.colors(255), range = c(0, 1), main = "Ground Reflectance")
 dev.off()
 
-png(file.path(out_dir, "Vm.png"))
-plot(terra::unwrap(soilc$Vm), main = expression("Volumetric Mineral Fraction (m"^3*"/m"^3*")"))
+png(file.path(figs_dir, "rho.png"))
+plot(terra::unwrap(ptm_soilc$rho), main = expression("Bulk Density (Mg/m"^3*")"))
 dev.off()
 
-png(file.path(out_dir, "Vq.png"))
-plot(terra::unwrap(soilc$Vq), main = expression("Volumetric Quartz Fraction (m"^3*"/m"^3*")"))
+png(file.path(figs_dir, "Vm.png"))
+plot(terra::unwrap(ptm_soilc$Vm), main = expression("Volumetric Mineral Fraction (m"^3*"/m"^3*")"))
 dev.off()
 
-png(file.path(out_dir, "Mc.png"))
-plot(terra::unwrap(soilc$Mc), main = expression("Mass Fraction of Clay (kg/kg)"))
+png(file.path(figs_dir, "Vq.png"))
+plot(terra::unwrap(ptm_soilc$Vq), main = expression("Volumetric Quartz Fraction (m"^3*"/m"^3*")"))
+dev.off()
+
+png(file.path(figs_dir, "Mc.png"))
+plot(terra::unwrap(ptm_soilc$Mc), main = expression("Mass Fraction of Clay (kg/kg)"))
 dev.off()
